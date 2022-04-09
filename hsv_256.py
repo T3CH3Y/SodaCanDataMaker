4983b021152a4ee5c37b8ea8c5c02bddb4be7f17
Ben Zimmerman <benzimmerman190@gmail.com>
2022-04-09 16:13:10 -0400

    got dots now woo

--- hsv_256.py
+++ hsv_256.py
@@ -3,7 +3,7 @@ import cv2
 
 def hsv_256(hsv100):
     hsv256 = []
-    hsv256.append(int(hsv100[0] * 3.6))
+    hsv256.append(int(hsv100[0] * 255 / 360))
     hsv256.append(int(hsv100[1] * 255 / 100))
     hsv256.append(int(hsv100[2] * 255 / 100))
 
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+from cv2 import COLOR_BGR2GRAY
 import numpy as np
 import cv2
 from hsv_256 import hsv_256
+import time
 # detects coke can
 # it works woo
 cap = cv2.VideoCapture(0)
 
+width = int(cap.get(3))
+height = int(cap.get(4))
+contrast = np.zeros((height, width, 3), np.uint8)
+contrast[:] = (0, 0, 0)
+
+
 while True:
     ret, frame = cap.read()
-    width = int(cap.get(3))
-    height = int(cap.get(4))
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
@@ -19,12 +25,21 @@ while True:
 
     mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
     mask2= cv2.inRange(hsv, lower_red2, upper_red2)
-
     mask = cv2.bitwise_or(mask1, mask2)
-    result = cv2.bitwise_and(frame, frame, mask=mask)
+    royalmask = cv2.bitwise_and(frame, frame, mask=mask)
 
-    cv2.imshow('sodas', result)
+    blackroyal = cv2.cvtColor(royalmask, COLOR_BGR2GRAY)
+    corners = cv2.goodFeaturesToTrack(blackroyal, 20, 0.01, 20)
+    if (corners is not None):
+        corners = np.int0(corners)
+    else:
+        corners = np.array([])
 
+    for corner in corners:
+        x, y = corner.ravel()
+        cv2.circle(royalmask, (x,y), 5, (255,255,255), -1)
+
+    cv2.imshow('sodas', royalmask)
     if cv2.waitKey(1) == ord('q'):
         break
 cap.release()
